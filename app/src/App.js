de46faa8c6a5426c75c0839645927458ed013c53
Nicolas Chambrier <naholyr@gmail.com>
2021-04-12 19:06:49 +0200

    J2 (prepare): move j1 components to src/j1/

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,7 @@
-// import logo from './logo.svg';
-import { useState } from "react";
 import "./App.css";
-import { CounterList } from "./CounterList/CounterList";
 
 function App() {
-  const [counterListKey, setCounterListKey] = useState(1);
-  const onClickReset = (e) => {
-    e.preventDefault();
-    setCounterListKey(counterListKey + 1);
-  };
-
-  return (
-    <div className="App">
-      {/* process.env.REACT_APP_WHATEVER */}
-      <CounterList key={counterListKey} />
-      <button onClick={onClickReset}>reset</button>
-    </div>
-  );
+  return null;
 }
 
 export default App;
